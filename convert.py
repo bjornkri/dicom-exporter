aa8214ccfd63e93613663d68dd7e9427e5f389ff
Björn Kristinsson <bjorn@bjornkri.com>
2022-08-11 15:38:48 +0200

    Dynamically find the volume filename

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
-from curses import window
+import glob
+import json
 import os
 import requests
 import typer
@@ -15,6 +16,7 @@ CASESLIST = '/cases/'
 STUDYRESOURCELIST = '/study-resources/'
 TOKEN = os.getenv('TOKEN')
 SERVER = os.getenv('SERVER')
+HEADERS = {'Authorization': f'Token {TOKEN}'}
 
 
 def get_study_id(case_identifier: str):
@@ -23,10 +25,10 @@ def get_study_id(case_identifier: str):
         TextColumn("[progress.description]{task.description}"),
     ) as progress:
         get_case = progress.add_task(description='Fetching case...', total=1)
-        headers = {'Authorization': f'Token {TOKEN}'}
+
         case_r = requests.get(f'{SERVER}{CASESLIST}',
             params={'case_identifier': case_identifier},
-            headers=headers)
+            headers=HEADERS)
         study_id = case_r.json()['results'][0]['studyId']
         progress.advance(get_case)
     return study_id
@@ -38,10 +40,9 @@ def retrieve_pseudo_dicom_url(study_id: str):
         TextColumn("[progress.description]{task.description}"),
     ) as progress:
         get_url = progress.add_task(description='Locating DICOM...', total=1)
-        headers = {'Authorization': f'Token {TOKEN}'}
         studyresource_r = requests.get(f'{SERVER}{STUDYRESOURCELIST}',
             params={'type': 1, 'study_id': study_id},
-            headers=headers)
+            headers=HEADERS)
         studyresource_url = studyresource_r.json()['results'][0]['readUrl']
         progress.advance(get_url)
     return studyresource_url
@@ -95,15 +96,53 @@ def determine_levels(dicom_directory):
     return (window_center, window_width)
 
 
+def create_study_resource(study_id, levels):
+    with open('outputs/output.vtkjs/index.json', 'r') as f, Progress(
+            SpinnerColumn(finished_text="✔️"),
+            TextColumn("[progress.description]{task.description}"),
+        ) as progress:
+        resource = progress.add_task("Creating studyResource...", total=1)
+        attribs = json.load(f)
+        attribs['levels'] = [levels[0], levels[1]]
+        data = {
+            'type': 2, 'studyId': study_id, 'attributes': attribs,
+            'metadata': {
+                'mime_type': 'application/gzip',
+            },
+            'name': 'vtkjs',
+        }
+        studyresource_r = requests.post(f'{SERVER}{STUDYRESOURCELIST}',
+            json=data,
+            headers=HEADERS)
+        progress.advance(resource)
+    return studyresource_r.json()
+
+
+def upload_volume_to(url):
+    path = 'outputs/output.vtkjs/data/'
+    name = glob.glob(f'{path}*.gz')[0].split('/')[-1]
+
+    with open(f'{path}{name}', 'rb') as f, Progress(
+        SpinnerColumn(finished_text="✔️"),
+        TextColumn("[progress.description]{task.description}"),
+    ) as progress:
+        upload = progress.add_task("Uploading vtkjs...", total=1)
+        files = { 'file': (
+            name, f)
+        }
+        requests.put(
+            url, files=files,
+        )
+        progress.advance(upload)
+
+
 @app.command()
 def main(case_identifier: str):
     study_id = get_study_id(case_identifier)
     url = retrieve_pseudo_dicom_url(study_id)
     dicom_filename = download_pseudo_dicom(case_identifier, url)
-    # dicom_filename = f'{case_identifier}.zip'
     extracted_dicom_path = extract(dicom_filename)
     levels = determine_levels(extracted_dicom_path)
-    print(levels)
     with Progress(
         SpinnerColumn(finished_text="✔️"),
         TextColumn("[progress.description]{task.description}"),
@@ -115,6 +154,8 @@ def main(case_identifier: str):
             overwrite=True
         )
         progress.advance(convert)
+    sr = create_study_resource(study_id, levels)
+    upload_volume_to(sr['writeUrl'])
 
 
 if __name__ == '__main__':
